58c4dad7c941c72035fedf6ad8cc7cff73e3202b
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-01-25 20:10:22 -0300

    refactor: connect database with prisma

---
+++ src/repository/carRepository.ts
@@ -0,0 +1,58 @@
+import { prisma } from "../config/database.js";
+
+async function getCars() {
+  const data = await prisma.car.findMany();
+  return data;
+}
+
+async function getCar(id: number) {
+  const data = await prisma.car.find({
+    where: {
+      id: id,
+    },
+  });
+  return data;
+}
+
+async function getCarWithLicensePlate(licensePlate: string) {
+  const data = await prisma.car.findFirst({
+    where: {
+      licensePlate: licensePlate,
+    },
+  });
+  return data;
+}
+
+async function createCar(
+  model: string,
+  licensePlate: string,
+  year: number,
+  color: string
+) {
+  await prisma.car.create({
+    data: {
+      model,
+      licensePlate,
+      year,
+      color,
+    },
+  });
+}
+
+async function deleteCar(id: number) {
+  await prisma.car.delete({
+    where: {
+      id: id,
+    },
+  });
+}
+
+const carRepository = {
+  getCar,
+  getCarWithLicensePlate,
+  getCars,
+  createCar,
+  deleteCar,
+};
+
+export default carRepository;
